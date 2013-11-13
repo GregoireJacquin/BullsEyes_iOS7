aa01d8d46ab0c0a1e3dc261e1bbd7315cf200b16
Mac Dev <macpao2@iMac-de-macdev.local>
2013-11-13 12:02:12 +0100

    Initial Commit

---
+++ BullsEyes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BullsEyes
+//
+//  Created by Mac Dev on 13/11/2013.
+//  Copyright (c) 2013 RetroTouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
